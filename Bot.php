6e2a0acec9d8ac25dfb1820d8ac7a6fe485f0a20
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-05-24 12:49:12 +0430

    fixed some bugs , added send message

--- Bot.php
+++ Bot.php
@@ -4,11 +4,13 @@ final class Bot
 {
     private $token;
     private $api;
+    protected $useWebhook;
 
-    public function __construct(string $TOKEN)
+    public function __construct(string $TOKEN, bool $useWebhook = false)
     {
         $this->token = $TOKEN;
         $this->api = 'https://api.telegram.org/bot'.$TOKEN.'/';
+        $this->useWebhook = $useWebhook;
     }
     //main Commands
     public function getMe() :stdClass
@@ -17,7 +19,16 @@ final class Bot
     }
     public function getUpdates() :stdClass
     {
-        return json_decode(file_get_contents($this->method('getUpdates')));
+        $destination = ($this->useWebhook) ? "php://input" :$this->method('getUpdates');
+        return json_decode(file_get_contents($destination));
+    }
+    public function sendMessage(string $text , string $chatId) : bool 
+    {
+        $res = file_get_contents($this->method('sendMessage' , [
+            'chat_id' => $chatId,
+            'text' => $text
+        ]));
+        return ($res != false) ? true : false;
     }
     //make methods easy to use
     private function method(string $method, array $params = null): string
@@ -26,7 +37,7 @@ final class Bot
         if (!empty($params)) {
             $res .= '?';
             foreach ($params as $param => $value) {
-                $res .= $param.'='.$value;
+                $res .= trim($param).'='.trim($value);
                 if (array_key_last($params) != $param) {
                     $res .= '&';
                 }
--- index.php
+++ index.php
@@ -7,5 +7,6 @@ define('TOKEN', '1767056535:AAHluR8Kblu_bg9MR67ztsVE26Vdzc7GMI8');
 
 require_once(__DIR__ ."/Bot.php");
 //main executable program 
-$bot = new Bot(TOKEN);
-echo $bot->getUpdates();
+$bot = new Bot(TOKEN,true);
+$update = $bot->getUpdates();
+$bot->sendMessage($update->message->text , $update->message->chat->id);
