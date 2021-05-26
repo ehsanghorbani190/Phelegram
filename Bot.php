8204f4551f3712f71c995e6d1b56698696923067
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-05-26 18:30:20 +0430

    Added sendPhotoByID ,debug funcs

--- Bot.php
+++ Bot.php
@@ -1,43 +1,77 @@
 <?php
+
 //Bot class
 final class Bot
 {
     private $token;
-    private $api;
-    protected $useWebhook;
+    private $useWebhook;
+    private $debugID;
 
-    public function __construct(string $TOKEN, bool $useWebhook = false)
+    public function __construct(string $TOKEN, string $debugID = null , bool $useWebhook = true)
     {
         $this->token = $TOKEN;
-        $this->api = 'https://api.telegram.org/bot'.$TOKEN.'/';
+        define('API', 'https://api.telegram.org/bot'.$TOKEN.'/');
         $this->useWebhook = $useWebhook;
+        if (null !== $debugID) {
+            $this->debugID = $debugID;
+        }
     }
+
     //main Commands
-    public function getMe() :stdClass
+    public function getMe(): stdClass
     {
         return json_decode(file_get_contents($this->method('getMe')));
     }
-    public function getUpdates() :stdClass
+
+    public function getUpdates(): stdClass
     {
-        $destination = ($this->useWebhook) ? "php://input" :$this->method('getUpdates');
+        $destination = ($this->useWebhook) ? 'php://input' : $this->method('getUpdates');
+
         return json_decode(file_get_contents($destination));
     }
-    public function sendMessage(string $text , string $chatId) : bool 
+
+    public function sendMessage(string $text, string $chatId): bool
     {
-        $res = file_get_contents($this->method('sendMessage' , [
+        $res = file_get_contents($this->method('sendMessage', [
             'chat_id' => $chatId,
-            'text' => $text
+            'text' => $text,
         ]));
-        return ($res != false) ? true : false;
+
+        return (false != $res) ? true : false;
     }
-    public function getFile(string $fileID , string $fileName){
-        $fileData = json_decode(file_get_contents($this->method("getFile" , ['file_id' => $fileID])));
-        copy("https://api.telegram.org/file/bot".$this->token."/". $fileData->result->file_path, __DIR__."/".$fileName);
+
+    public function getFile(string $fileID, string $fileName)
+    {
+        $fileData = json_decode(file_get_contents($this->method('getFile', ['file_id' => $fileID])));
+        copy('https://api.telegram.org/file/bot'.$this->token.'/'.$fileData->result->file_path, __DIR__.'/'.$fileName);
+    }
+    public function sendPhotoByID(string $fileID , string $chatID , string $caption = '') : stdClass
+    {
+        return json_decode(file_get_contents($this->method('sendPhoto', [
+            'chat_id' => $chatID,
+            'photo' => $fileID,
+            'caption' => $caption
+        ])));
+    }
+    //testing functions
+    public static function storeInJson(stdClass $update): bool
+    {
+        $file = fopen($update->update_id.'.json', 'w');
+
+        return fwrite($file, json_encode($update));
+    }
+
+    public function debug(string $text): bool
+    {
+        return $this->sendMessage("***DEBUG LOG***".PHP_EOL.$text, $this->debugID);
+    }
+    public function setDebugID(string $debugID){
+        $this->debugID = $debugID;
     }
     //make methods easy to use
     private function method(string $method, array $params = null): string
     {
-        $res = $this->api.$method;
+        $res = API.$method;
         if (!empty($params)) {
             $res .= '?';
             foreach ($params as $param => $value) {
@@ -47,12 +81,7 @@ final class Bot
                 }
             }
         }
-        return $res;
-    }
-    //testing functions 
-    public static function storeInJson(stdClass $update) : bool{
-        $file = fopen($update->update_id.".json" , 'w');
-        $res = fwrite($file,json_encode($update));
+
         return $res;
     }
 }
