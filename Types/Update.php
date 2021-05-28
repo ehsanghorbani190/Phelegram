dd967fd5fddc3c1a4a7dbd3da801fc84d30b2054
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-05-29 00:20:35 +0430

    made little changes

--- Types/Update.php
+++ Types/Update.php
@@ -1,5 +1,10 @@
 <?php
 namespace Types\Update;
+use stdClass;
 class Update{
-    
+    public $id;
+    public $object;
+    public function __construct(stdClass $update) {
+        $this->id = $update->update_id;
+    }
 }
