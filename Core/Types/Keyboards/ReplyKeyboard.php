425a614caae178587fc41a8ed3502133afab0f8a
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-07-24 23:05:03 +0430

    Added ReplyKeyboard type

---
+++ Core/Types/Keyboards/ReplyKeyboard.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Phelegram\Core\Types\Keyboards;
+
+class ReplyKeyboard
+{
+    public $keyboard = [];
+    public $resize_keyboard = false;
+    public $one_time_keyboard = false;
+    public $selective = false;
+    public $input_field_placeholder;
+
+    /**
+     * A Keyboard that replies data and makes user interaction easier.
+     *
+     * @param string $placeholder will appear in the inputField while using keyboard
+     */
+    public function __construct(string $placeholder = null)
+    {
+        if (null != $placeholder) {
+            $this->input_field_placeholder = $placeholder;
+        }
+    }
+
+    /**
+     * If set, Telegram will try to resize the row for best fit.
+     */
+    public function setResizable(): self
+    {
+        $this->resize_keyboard = true;
+
+        return $this;
+    }
+
+    /**
+     * If set, the keyboard will hide after use.
+     */
+    public function setOnceOnly(): self
+    {
+        $this->one_time_keyboard = true;
+
+        return $this;
+    }
+
+    /**
+     * If set, the keyboard will hide only for user that used it.
+     */
+    public function setSelective(): self
+    {
+        $this->selective = true;
+
+        return $this;
+    }
+
+    /**
+     * Add a simple text button in the specified row. Returns the text on click as a message.
+     *
+     * @param string $text Label of button
+     * @param int    $row  Row to insert button into, starts from 0, default is set to 0
+     */
+    public function addButton(string $text, int $row = 0): self
+    {
+        $this->keyboard[$row][] = new ReplyKeyboardButton($text);
+
+        return $this;
+    }
+
+    /**
+     * Adds a button that sends the user's Contact on click, in the specified row. ONLY FOR PRIVATE CHATS.
+     *
+     * @param string $text Label of button
+     * @param int    $row  Row to insert button into, starts from 0, default is set to 0
+     */
+    public function addContactButton(string $text, int $row = 0): self
+    {
+        $button = new ReplyKeyboardButton($text);
+        $this->keyboard[$row][] = $button->requestContact();
+
+        return $this;
+    }
+
+    /**
+     * Adds a button that sends the user's Location on click, in the specified row. ONLY FOR PRIVATE CHATS.
+     *
+     * @param string $text Label of button
+     * @param int    $row  Row to insert button into, starts from 0, default is set to 0
+     */
+    public function addLocationButton(string $text, int $row = 0): self
+    {
+        $button = new ReplyKeyboardButton($text);
+        $this->keyboard[$row][] = $button->requestLocation();
+
+        return $this;
+    }
+}
