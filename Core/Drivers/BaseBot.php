a54c8413578fd4dda0e8bd781dcf79ad516ba59d
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-08-01 18:09:05 +0430

    Added comments to BaseBot Driver

--- Core/Drivers/BaseBot.php
+++ Core/Drivers/BaseBot.php
@@ -9,6 +9,9 @@ use Phelegram\Core\Types\Sender\User;
 use Phelegram\Utilities\Curl;
 use Phelegram\Utilities\Env;
 
+/**
+ * Contains basic actions that every bot in Telegram should be able to do.
+ */
 class BaseBot
 {
     private string $debugID;
@@ -20,18 +23,32 @@ class BaseBot
         $this->request = new Curl();
     }
 
+    /**
+     * A simple function to check if your token is still valid.
+     *
+     * @return User Your bot information
+     */
     public function getMe(): User
     {
         return new User(json_decode($this->request->getMethod('getMe')));
     }
 
+    /**
+     * Getting an update from WebHook, The update will be marked as read.
+     *
+     * @return Update The update sent to bot from WebHook
+     */
     public function getUpdate(): Update
     {
         return new Update(file_get_contents('php://input'));
     }
 
     /**
-     * @return Update[]
+     * Getting UNREAD updates and mark them as read. WON'T WORK IF WEBHOOK IS SET.
+     *
+     * @param int $limit Number of updates to return. Max is 100. Default is 100.
+     *
+     * @return Update[] Unread Updates
      */
     public function getUpdates(int $limit = 100)
     {
@@ -43,6 +60,15 @@ class BaseBot
         }
     }
 
+    /**
+     * Send a simple text message to a chat with optional reply_markup(ReplyKeyboard, RemoveReplyKeyboard).
+     *
+     * @param string   $text     the text to send
+     * @param string   $chatId   ID of the chat to send message in ,In case of Channels it would be like @ChannelUserName
+     * @param Keyboard $keyboard reply_markup to send, see their description when you create one
+     *
+     * @return bool shows if message was sent or not
+     */
     public function sendMessage(string $text, string $chatId, Keyboard $keyboard = null): bool
     {
         $options = [
@@ -57,6 +83,14 @@ class BaseBot
         return json_decode($res)->ok;
     }
 
+    /**
+     * Deletes an existing message from a chat.
+     *
+     * @param string $chatID    Chat that contains message
+     * @param string $messageID ID of the message in chat
+     *
+     * @return bool shows if message is deleted or not
+     */
     public function deleteMessage(string $chatID, string $messageID): bool
     {
         return json_decode($this->request->getMethod('deleteMessage', [
@@ -65,6 +99,13 @@ class BaseBot
         ]))->ok;
     }
 
+    /**
+     * Get info about a file that exists on Telegram clouds.
+     *
+     * @param string $fileID The file_id (not file_unique_id) returned from Telegram API(in a message)
+     *
+     * @return File A File Object that can be used to download a file form Telegram
+     */
     public function getFile(string $fileID): File
     {
         $fileData = json_decode($this->request->getMethod('getFile', ['file_id' => $fileID]));
@@ -72,7 +113,11 @@ class BaseBot
         return new File($fileData->result);
     }
 
-    //testing functions
+    /**
+     * Stores the update in a json file in case that you want to debug or develop your bot. file name will be the update id.
+     *
+     * @param Update $update the Update to write in file
+     */
     public static function storeInJson(Update $update): bool
     {
         $file = fopen($update->update_id.'.json', 'w');
@@ -80,6 +125,13 @@ class BaseBot
         return fwrite($file, json_encode($update));
     }
 
+    /**
+     * Send a Text to $DEBUG account you added in .env file. It will start with "***DEBUG LOG***". Useful for debugging.
+     *
+     * @param string $text The text to send
+     *
+     * @return bool shows if message was sent or not
+     */
     public function debug(string $text): bool
     {
         return ('null' != $this->debugID) ? $this->sendMessage(urlencode("***DEBUG LOG*** \n".$text), $this->debugID) : false;
